05b2ca07199a86ff1af3800e1f28235a519eca23
Stuart Lee <stuart.lee@drpgroup.com>
2016-03-16 10:50:50 +0000

    handle comments when parsing

--- src/parse.js
+++ src/parse.js
@@ -7,20 +7,61 @@ function parse(text) {
 
   var story = new Story();
 
+  var _inComment = false;
+
   lines.forEach(function(line) {
 
-    if(isComment(line)) {
+    line = line.trim();
+
+    // blank line
+    if(line === '') { return; }
+
+    if(isMultiLineCommentEnd(line)) {
+      _inComment = false;
+      return;
+    }
+
+    if(isMultiLineCommentStart(line)) {
+      _inComment = true;
+      return;
+    }
+
+    if(isSingleLineComment(line)) {
+      return;
+    }
+
+    if(isTodo(line)) {
+      return;
+    }
+
+    if(_inComment) {
       return;
     }
 
     var node = new Node();
+
     node.content = line;
+
     story.stack.unshift(node);
   });
 
   return story;
 }
 
-isComment()
+function isSingleLineComment(line) {
+  return line.startsWith('//');
+}
+
+function isMultiLineCommentStart(line) {
+  return line.startsWith('/*');
+}
+
+function isMultiLineCommentEnd(line) {
+  return line.endsWith('*/');
+}
+
+function isTodo(line) {
+  return line.startsWith('TODO');
+}
 
 module.exports = parse;
